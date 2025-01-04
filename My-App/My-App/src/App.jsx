cf3af0c143142e3ad825f63fbc59abdce29516c9
maxtrosy <maxtrosy@gmail.com>
2025-01-03 23:13:56 -0500

    a

--- My-App/My-App/src/App.jsx
+++ My-App/My-App/src/App.jsx
@@ -9,6 +9,8 @@ import Blog from "./components/Blog";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import bgImage from "./assets/bgq.jpg";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 import "./App.css";
 
