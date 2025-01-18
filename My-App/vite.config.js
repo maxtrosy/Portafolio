6ef0482171c5f07e0abf578021bbb51024c42e97
maxtrosy <maxtrosy@gmail.com>
2025-01-18 15:35:50 -0500

    XSD

--- My-App/vite.config.js
+++ My-App/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000, // Usa el puerto proporcionado por Render o el puerto 3000 por defecto
-    host: '0.0.0.0', // Asegúrate de que Vite escuche en todas las interfaces
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0', 
   },
 })
