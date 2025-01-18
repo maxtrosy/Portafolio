ff1f0933b2b258c098aeea61f98cf81c44accc75
maxtrosy <maxtrosy@gmail.com>
2025-01-18 15:27:16 -0500

    PORT SETT

--- My-App/vite.config.js
+++ My-App/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000, // Usa el puerto proporcionado por Render o el puerto 3000 por defecto
+    host: '0.0.0.0', // Asegúrate de que Vite escuche en todas las interfaces
+  },
 })
