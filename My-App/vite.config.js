e2b42af70e10fa68a6a48c33c3d86d8392844084
maxtrosy <maxtrosy@gmail.com>
2025-02-03 01:02:58 -0500

    Configuración para GitHub Pages

--- My-App/vite.config.js
+++ My-App/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portafolio/', // Asegura que coincida con el nombre de tu repositorio
   server: {
     port: process.env.PORT || 3000,
-    host: '0.0.0.0', 
+    host: '0.0.0.0',
   },
+  build: {
+    outDir: 'dist',
+  }
 })
