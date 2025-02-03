1c779c646d46fe97ea06728524e4d0d83b66df4a
maxtrosy <maxtrosy@gmail.com>
2025-02-03 01:38:10 -0500

    fg

--- My-App/vite.config.js
+++ My-App/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portafolio/', 
+  base: '/Portafolio/',  
   server: {
     port: process.env.PORT || 3000,
     host: '0.0.0.0',
